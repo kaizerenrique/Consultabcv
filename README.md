96445041620bd2c44ddb81f105fa02a8a4d61a00
kaizerenrique <kayserenrique@gmail.com>
2022-07-23 16:15:09 -0400

    Primer comentario

---
+++ README.md
@@ -0,0 +1,26 @@
+# Consultar el valor del USD
+## Consultabcv
+
+Este paquete desarrollado en PHP realiza una consulta del valor del USD publicado en el sitio web del Banco Central de Venezuela 
+
+### Requiere de
+* [guzzlehttp/guzzle: ^7.4](https://packagist.org/packages/guzzlehttp/guzzle)
+
+### Instalación 
+```php
+composer require kaizerenrique
+```
+### :scroll: Ejemplos 
+
+```
+/**
+* Esta función no recibe ningún parámetro 
+*
+* @return Retorna un número decimal.
+*/
+
+public function valorbcv()
+```
+
+#### ✒️ Autores 
+* [kaizerenrique](https://github.com/kaizerenrique)
---
+++ src/Consultabcv.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace Kaizerenrique\Consultabcv;
+
+use Illuminate\Support\Facades\Http;
+
+class Consultabcv{
+
+	/**
+	* Esta función no recibe ningún parámetro 
+	*
+	* @return Retorna un número decimal.
+	*/
+
+	public function valorbcv()
+    {
+	    try {
+	    	$url = 'http://bcv.org.ve/';
+
+	    	$response = Http::get($url);
+	    	$respuesta = $response->getBody()->getContents();// accedemos a el contenido
+			$text = strip_tags($respuesta); //limpiamos
+
+			$findme = 'USD'; 
+			$pos = strpos($text, $findme);
+
+			$rempl = array('USD');
+
+		    $r = trim(str_replace($rempl, '|', self::limpiarCampo($text)));
+		    $resource = explode("|", $r);
+		    $datos = explode(" ", self::limpiarCampo($resource[4])); 
+
+		    $usd = $datos[0]; //obtenemos el valor del USD
+		    $num=str_replace(',','.',$usd); //reemplazamos la coma por un punto
+		    $valor = floatval($num); //convertimos en un numero
+	    	return  $valor;
+
+    	} catch (\Illuminate\Http\Client\ConnectionException $e) {
+	        report($e);	 
+	        return false;
+	        
+	    }
+    }
+
+    public static function limpiarCampo($valor) {//Con esto limpiamos los errores de la pagina
+        $rempl = array('\n', '\t');
+        $r = trim(str_replace($rempl, ' ', $valor));
+        return str_replace("\r", "", str_replace("\n", "", str_replace("\t", "", $r)));
+    }
+}
